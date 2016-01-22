012c94b7a835a4ba161967601abbb725e32eb623
bowfinger <bowfinger2k1@hotmail.com>
2016-01-22 12:43:14 +0000

    New Stream Consumer to publish data via MQTT

---
+++ Monitoring/src/PackageData.java
@@ -0,0 +1,50 @@
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.opencv.core.MatOfPoint;
+
+import java.util.Date;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by Jamie on 22/01/2016.
+ */
+public class PackageData implements Consumer<List<MatOfPoint>> {
+    private Messenger messenger;
+    private int floorId;
+    public PackageData(int floorId) throws MqttException {
+        this.messenger = new Messenger("", new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                System.out.println("Message sent by OpenCV");
+            }
+        });
+        this.floorId = floorId;
+        messenger.connect();
+    }
+
+    @Override
+    public void accept(List<MatOfPoint> matOfPoints) {
+        FloorData data = new FloorData(floorId, new Date(), matOfPoints.size());
+
+        //publish first data then sleep for 10 seconds
+        try {
+            messenger.publish(String.format("Monitor/%d", floorId), data);
+            Thread.sleep(10000);
+        } catch (MqttException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
